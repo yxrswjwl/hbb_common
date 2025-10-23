595d6d68d6fbbcefb7df2941b6d5d1c8965574c3
yxrswjwl <46105937+yxrswjwl@users.noreply.github.com>
2025-10-23 15:37:41 +0800

    配置

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["xiongtieshu.top"];
+pub const RS_PUB_KEY: &str = "lhMPU5GJjVV23Jwu5jrBHfRKy6j+RJLciMqcz9vOEd4=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
